a65d19678b2536025665bb50f98fdde37e560b20
Yuan Yiyang <yuan3y@gmail.com>
2016-02-25 14:01:38 +0800

    modified default database

--- lib/mysql_template.php
+++ lib/mysql_template.php
@@ -8,7 +8,7 @@ class MySQL {
 
   public static function getInstance() {
     if (!self::$instance) {
-      self::$instance = new PDO('mysql:host=localhost;dbname=teamio', 'teamio', 'team10');;
+      self::$instance = new PDO('mysql:host=localhost;dbname=teamio', 'root', '');; //change database name, username, password to your own
       self::$instance->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     }
     return self::$instance;
