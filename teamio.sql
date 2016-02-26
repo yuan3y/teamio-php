64273cda54eab012df2cd60e3c280da3fcf341fa
Yuan Yiyang <yuan3y@gmail.com>
2016-02-26 08:23:30 +0800

    database snapshot

--- teamio.sql
+++ teamio.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Feb 18, 2016 at 11:20 PM
+-- Generation Time: Feb 26, 2016 at 08:07 AM
 -- Server version: 5.5.47-0ubuntu0.14.04.1
 -- PHP Version: 5.5.9-1ubuntu4.14
 
@@ -19,7 +19,7 @@ SET time_zone = "+00:00";
 --
 -- Database: `teamio`
 --
-CREATE DATABASE IF NOT EXISTS `teamio` DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci;
+CREATE DATABASE IF NOT EXISTS `teamio` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 USE `teamio`;
 
 -- --------------------------------------------------------
@@ -28,15 +28,13 @@ USE `teamio`;
 -- Table structure for table `comments`
 --
 
-DROP TABLE IF EXISTS `comments`;
-CREATE TABLE IF NOT EXISTS `comments` (
-  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+CREATE TABLE `comments` (
+  `id` int(11) UNSIGNED NOT NULL,
   `diary_id` int(11) NOT NULL,
   `name` varchar(25) NOT NULL DEFAULT '',
   `body` text NOT NULL,
-  `posted` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+  `posted` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `comments`
@@ -52,27 +50,245 @@ INSERT INTO `comments` (`id`, `diary_id`, `name`, `body`, `posted`) VALUES
 -- Table structure for table `diaries`
 --
 
-DROP TABLE IF EXISTS `diaries`;
-CREATE TABLE IF NOT EXISTS `diaries` (
-  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+CREATE TABLE `diaries` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `user_id` int(11) UNSIGNED NOT NULL,
   `title` varchar(25) NOT NULL DEFAULT '',
   `slug` varchar(25) NOT NULL,
   `body` text NOT NULL,
-  `published` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `slug` (`slug`)
-) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8;
+  `published` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `diaries`
 --
 
-INSERT INTO `diaries` (`id`, `title`, `slug`, `body`, `published`) VALUES
-(1, 'First Post', 'first-post', 'This is the first post for Teamio. Hello, world?', '2012-08-18 08:28:10'),
-(2, 'Second Post', 'second-post', 'Just another post to test out some features.\n\nLine break and *asterisks* to show Markdown integration.', '2012-08-18 08:39:03'),
-(14, 'One More Post', 'one-more-post', 'No need Content-Type, Posted from my browser', '2016-02-18 14:44:03'),
-(16, 'One More Post', 'one-more-post2', 'No need Content-Type, Posted from my browser', '2016-02-18 14:57:03');
+INSERT INTO `diaries` (`id`, `user_id`, `title`, `slug`, `body`, `published`) VALUES
+(1, 1, 'First Post', 'first-post', 'This is the first post for Teamio. Hello, world?', '2012-08-18 08:28:10'),
+(2, 1, 'Second Post', 'second-post', 'Just another post to test out some features.\n\nLine break and *asterisks* to show Markdown integration.', '2012-08-18 08:39:03'),
+(3, 1, 'One More Post', 'one-more-post', 'No need Content-Type, Posted from my browser', '2016-02-18 14:44:03'),
+(4, 1, 'One More Post', 'one-more-post2', 'No need Content-Type, Posted from my browser', '2016-02-18 14:57:03');
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `images`
+--
+
+CREATE TABLE `images` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `friend_name` varchar(30) NOT NULL,
+  `description` text,
+  `filename` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `images`
+--
+
+INSERT INTO `images` (`id`, `user_id`, `friend_name`, `description`, `filename`) VALUES
+(1, 1, 'Samuel', 'He reads minds.', '06c0153d.jpg'),
+(2, 1, 'Yuan Yiyang', 'He codes.', 'ee6a9fdc.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `oauth_access_tokens`
+--
+
+CREATE TABLE `oauth_access_tokens` (
+  `access_token` varchar(40) NOT NULL,
+  `client_id` varchar(80) NOT NULL,
+  `user_id` varchar(255) DEFAULT NULL,
+  `expires` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `scope` varchar(2000) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `oauth_access_tokens`
+--
+
+INSERT INTO `oauth_access_tokens` (`access_token`, `client_id`, `user_id`, `expires`, `scope`) VALUES
+('739220e770bc57c8b025b63c5fa3b747e4e215ed', 'testclient', NULL, '2016-02-25 07:49:02', NULL),
+('820300dc6a7a30911bab07c94d6a507c2f8e463c', 'testclient', NULL, '2016-02-25 07:49:07', NULL),
+('826482baa61a7887489084f99e4e6924e32f130c', 'testclient', NULL, '2016-02-25 07:56:14', NULL),
+('c807b031d429f0d9cae585d483a2c85db4b16b8f', 'testclient', NULL, '2016-02-25 07:50:46', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `oauth_authorization_codes`
+--
+
+CREATE TABLE `oauth_authorization_codes` (
+  `authorization_code` varchar(40) NOT NULL,
+  `client_id` varchar(80) NOT NULL,
+  `user_id` varchar(255) DEFAULT NULL,
+  `redirect_uri` varchar(2000) DEFAULT NULL,
+  `expires` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `scope` varchar(2000) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `oauth_authorization_codes`
+--
+
+INSERT INTO `oauth_authorization_codes` (`authorization_code`, `client_id`, `user_id`, `redirect_uri`, `expires`, `scope`) VALUES
+('0da74d721e1ab61e05b8d64fe0c4b8c34286e6f1', 'testclient', NULL, NULL, '2016-02-25 06:54:17', NULL),
+('67250f210c1c5a97627b06f3e105cd724d3f9ce7', 'testclient', NULL, NULL, '2016-02-25 06:55:06', NULL),
+('67a950f1c568510783fa5a12b78e1992cc580d3f', 'testclient', NULL, NULL, '2016-02-25 06:55:24', NULL),
+('c5a930b9809f1230b8833e58ec06aa3e3799ec45', 'testclient', NULL, NULL, '2016-02-25 06:54:07', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `oauth_clients`
+--
+
+CREATE TABLE `oauth_clients` (
+  `client_id` varchar(80) NOT NULL,
+  `client_secret` varchar(80) DEFAULT NULL,
+  `redirect_uri` varchar(2000) NOT NULL,
+  `grant_types` varchar(80) DEFAULT NULL,
+  `scope` varchar(100) DEFAULT NULL,
+  `user_id` varchar(80) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `oauth_clients`
+--
+
+INSERT INTO `oauth_clients` (`client_id`, `client_secret`, `redirect_uri`, `grant_types`, `scope`, `user_id`) VALUES
+('testclient', 'testpass', 'http://fake/', NULL, NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `oauth_jwt`
+--
+
+CREATE TABLE `oauth_jwt` (
+  `client_id` varchar(80) NOT NULL,
+  `subject` varchar(80) DEFAULT NULL,
+  `public_key` varchar(2000) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `oauth_refresh_tokens`
+--
+
+CREATE TABLE `oauth_refresh_tokens` (
+  `refresh_token` varchar(40) NOT NULL,
+  `client_id` varchar(80) NOT NULL,
+  `user_id` varchar(255) DEFAULT NULL,
+  `expires` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `scope` varchar(2000) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `oauth_scopes`
+--
+
+CREATE TABLE `oauth_scopes` (
+  `scope` text,
+  `is_default` tinyint(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `oauth_users`
+--
+
+CREATE TABLE `oauth_users` (
+  `username` varchar(255) NOT NULL,
+  `password` varchar(2000) DEFAULT NULL,
+  `first_name` varchar(255) DEFAULT NULL,
+  `last_name` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `comments`
+--
+ALTER TABLE `comments`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `diaries`
+--
+ALTER TABLE `diaries`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `images`
+--
+ALTER TABLE `images`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `oauth_access_tokens`
+--
+ALTER TABLE `oauth_access_tokens`
+  ADD PRIMARY KEY (`access_token`);
+
+--
+-- Indexes for table `oauth_authorization_codes`
+--
+ALTER TABLE `oauth_authorization_codes`
+  ADD PRIMARY KEY (`authorization_code`);
+
+--
+-- Indexes for table `oauth_clients`
+--
+ALTER TABLE `oauth_clients`
+  ADD PRIMARY KEY (`client_id`);
+
+--
+-- Indexes for table `oauth_jwt`
+--
+ALTER TABLE `oauth_jwt`
+  ADD PRIMARY KEY (`client_id`);
+
+--
+-- Indexes for table `oauth_refresh_tokens`
+--
+ALTER TABLE `oauth_refresh_tokens`
+  ADD PRIMARY KEY (`refresh_token`);
+
+--
+-- Indexes for table `oauth_users`
+--
+ALTER TABLE `oauth_users`
+  ADD PRIMARY KEY (`username`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `comments`
+--
+ALTER TABLE `comments`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT for table `diaries`
+--
+ALTER TABLE `diaries`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+--
+-- AUTO_INCREMENT for table `images`
+--
+ALTER TABLE `images`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
