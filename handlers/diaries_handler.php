2a377d11cf48b3aab6fa6271ff395c7f7b54b3c3
Yuan Yiyang <yuan3y@gmail.com>
2016-03-07 10:18:23 +0800

    correct function call parameter

--- handlers/diaries_handler.php
+++ handlers/diaries_handler.php
@@ -10,14 +10,12 @@ class diariesHandler
 
     function get_xhr($user_id)
     {
-        $diaries = get_diaries();
-//        include( $_SERVER["DOCUMENT_ROOT"] . "/views/diaries.php");
+        $diaries = get_diaries($user_id);
         _response($diaries);
     }
 
     function post_xhr($user_id)
     {
-//        echo json_encode($_POST);
         $params = _set_default('title', 'slug', 'body', 'published');
         new_diary($params['title'], $params['body'], $params['published'], $params['slug'], $user_id);
     }
