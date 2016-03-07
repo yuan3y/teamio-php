8de26b78065ec74972b2909653a5fd0832d7e659
Yuan Yiyang <yuan3y@gmail.com>
2016-03-07 10:02:00 +0800

    standardize uploads path definition

--- handlers/images_handler.php
+++ handlers/images_handler.php
@@ -2,11 +2,13 @@
 
 class imagesHandler
 {
-    function get($user_id){
+    function get($user_id)
+    {
         $this->get_xhr($user_id);
     }
 
-    function post($user_id){
+    function post($user_id)
+    {
         $this->post_xhr($user_id);
     }
 
@@ -18,12 +20,10 @@ class imagesHandler
 
     function post_xhr($user_id)
     {
-//        $params = _set_default('friend_names[]', 'descriptions[]');
         $params = _set_default();
-//        $params=$_POST();
         $allowedTypes = array(IMAGETYPE_PNG, IMAGETYPE_JPEG, IMAGETYPE_GIF);
-        $extensions = array(IMAGETYPE_PNG=>".png",IMAGETYPE_JPEG=>".jpg",IMAGETYPE_GIF=>".gif");
-        $uploads_dir = $_SERVER['DOCUMENT_ROOT'].'/uploads';
+        $extensions = array(IMAGETYPE_PNG => ".png", IMAGETYPE_JPEG => ".jpg", IMAGETYPE_GIF => ".gif");
+        $uploads_dir = $_SERVER['DOCUMENT_ROOT'] . UPLOADS_PATH;
         $result = array();
         foreach ($_FILES["pictures"]["error"] as $key => $error) {
             #http://php.net/manual/en/function.move-uploaded-file.php
@@ -33,9 +33,9 @@ class imagesHandler
                 $error = !in_array($detectedType, $allowedTypes);
                 #http://stackoverflow.com/questions/6755192/uploaded-file-type-check-by-php
                 if ($error) continue;
-                $name = hash_file('CRC32', $tmp_name , FALSE).$extensions[$detectedType];
-                move_uploaded_file($tmp_name, "$uploads_dir/$name");
-                $result[]=insert_image($user_id,$params['friend_names'][$key],$params['descriptions'][$key],$name);
+                $name = hash_file('CRC32', $tmp_name, FALSE) . $extensions[$detectedType];
+                move_uploaded_file($tmp_name, $uploads_dir . $name);
+                $result[] = insert_image($user_id, $params['friend_names'][$key], $params['descriptions'][$key], $name);
             }
         }
         _response($result);
