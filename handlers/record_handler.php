3acc68057b0b0ccc221896f394a4ab11e8c136b8
Yuan Yiyang <yuan3y@gmail.com>
2016-03-16 02:12:45 +0800

    post method to replace put for record

--- handlers/record_handler.php
+++ handlers/record_handler.php
@@ -23,4 +23,15 @@ class RecordHandler
         $params = _parsePut();
         _response(update_record($user_id,$id,$params['win']));
     }
+
+    function post($user_id,$id)
+    {
+        $this->post_xhr($user_id,$id);
+    }
+
+    function post_xhr($user_id,$id)
+    {
+        $params = _set_default();
+        _response(update_record($user_id,$id,$params['win']));
+    }
 }
